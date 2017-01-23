b98877230e1840bfb3ec4ee7521136d07459614d
Toby Paul <tasutagazo@gmail.com>
2017-01-23 16:59:21 +0800

    Add post for Todos

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,	
-	description: "Meet Najib for lunch",
-	completed: false
-}, {
-	id: 2,
-	description: "Give 2.6b",
-	completed: false
-}, {
-	id: 3,
-	description: "Cover your ass",
-	completed: true
-}];
+var todos = [];
+var todoNextID = 1;
 app.get('/', function(req, res){
 	res.send('Todo API Root');
 })
+app.use(bodyParser.json());
 // GET /todos
 app.get('/todos', function(req, res){
 	res.json(todos);
@@ -37,6 +28,25 @@ app.get('/todos/:id', function(req, res){
 		res.status(404).send('No such id');
 	}
 })
+// POST /Todos/:id
+app.post('/todos', function(req, res){
+	var body = req.body;
+	//add id field
+	body.id = todoNextID;
+	if(body.id = todoNextID){
+		todoNextID++
+	}
+	console.log(todoNextID);
+	// push body into array
+	todos.push(body)
+	console.log(todos)
+	console.log('description ' + body.description);
+	res.json(body);
+});
+
+
+
+
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT)
 })
