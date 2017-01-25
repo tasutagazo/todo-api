e24f1b04f01b85fe089fd8cb6a3901053ba987ba
Toby Paul <tasutagazo@gmail.com>
2017-01-25 18:50:16 +0800

    added completed filter for get todos

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.use(bodyParser.json());
 // GET /todos
 
 app.get('/todos', function(req, res){
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	// if has property & completed === true
+		if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+			filteredTodos = _.where(filteredTodos, {completed: true});
+		} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+			filteredTodos = _.where(filteredTodos, {completed: false});
+		} 
+
+	res.json(filteredTodos);
 });
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
